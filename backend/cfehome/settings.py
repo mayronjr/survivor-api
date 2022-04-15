c11855141984b81f5e78a861b1a2c79212745709
Mayron Junior <mayronjunior5@gmail.com>
2022-04-15 11:36:14 -0300

    Change DataBase from SQLite to PostgreSQL

--- backend/cfehome/settings.py
+++ backend/cfehome/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api',
+    'sobreviventes',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,17 @@ WSGI_APPLICATION = 'cfehome.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': 'mydb',
+
+        'USER': 'apisobrevivente',
+
+        'PASSWORD': '123456789',
+
+        'HOST': 'localhost',
+
+        'PORT': '5432',
     }
 }
 
