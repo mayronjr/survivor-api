8391773524e4437797fee67b875890bb4c317ba5
Mayron Junior <mayronjunior5@gmail.com>
2022-04-15 20:53:55 -0300

    Correct spelling errors Add new verifications in relate_infection Endpoint

--- backend/api/views.py
+++ backend/api/views.py
@@ -43,13 +43,19 @@ def api_update_survivor_location(request, id, *args, **kwargs):
     survivor.is_valid(raise_exception=True)
     survivor.save()
 
-    return Response(survivor.validated_data, status=status.HTTP_200_OK)
+    return Response({"message": "Localização atualizada"}, status=status.HTTP_200_OK)
 
 @api_view(['POST'])
 def api_relate_infection(request, *args, **kwargs):
     report = ReportSerializer(data=request.data)
 
     report.is_valid(raise_exception=True)
+    
+    if report.validated_data['reporting'].id == report.validated_data['reported'].id:
+        return Response({"message": "Um Sobrevivente não pode reportar a si mesmo."}, status=status.HTTP_400_BAD_REQUEST)
+    if report.validated_data['reporting'].is_infected:
+        return Response({"message": "Um infectado não pode reportar um Sobrevivente."}, status=status.HTTP_400_BAD_REQUEST)
+    
     try:
         report = report.save()
     except IntegrityError:
@@ -57,7 +63,7 @@ def api_relate_infection(request, *args, **kwargs):
     if Reports.objects.filter(reported=report.reported).count() >= 3 and not report.reported.is_infected:
         report.reported.is_infected = True
         report.reported.save(update_fields=['is_infected'])
-        return Response({"message": "Sobrivivente reportado como infectado."}, status=status.HTTP_201_CREATED)
+        return Response({"message": "Sobrevivente reportado como infectado."}, status=status.HTTP_201_CREATED)
     return Response({"message": "Report foi feito."}, status=status.HTTP_201_CREATED)
 
 @api_view(['PATCH'])
